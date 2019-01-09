cc643e9f477fdfde6bdf75e828d6d08a16b71119
AddisonPascal <addisondj@hotmail.com>
2019-01-09 16:25:45 +1100

    Made program
    Made program for making internet shortcuts.

---
+++ ShortcutMaker.bat
@@ -0,0 +1,37 @@
+@echo off
+title Batch Shortcut Maker v0.1
+goto home
+
+:home
+cls
+echo 1= Make Shortcut
+echo 2= Exit
+set/p option="--> "
+if "%option%"=="1" goto makeShortcut
+exit
+
+:makeShortcut
+cls
+echo Pick a type of shortcut: 
+echo 1= Internet Shortcut
+::echo 2= File Shortcut
+set /p shortcutType= "--> "
+if "%shortcutType%"=="1" goto internetShortcut
+::if "%shortcutType%"=="2" goto fileShortcut
+goto makeShortcut
+
+:internetShortcut
+cls
+echo Address:
+set/p address= "--> "
+echo Shortcut Name: 
+set/p name="--> "
+(
+@echo off
+echo start iexplore "%address%"
+)>"%name%.link.bat"
+cls
+echo Done!
+echo Press any key to exit. 
+pause>nul
+exit
