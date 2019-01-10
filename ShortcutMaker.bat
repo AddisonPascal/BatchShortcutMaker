7c34d08df28bc98a726d1df60c72bc48fdfbad9d
AddisonPascal <addisondj@hotmail.com>
2019-01-10 15:09:27 +1100

    Hopefully finished file shortcuts (not yet implemented)

--- ShortcutMaker.bat
+++ ShortcutMaker.bat
@@ -38,7 +38,7 @@ exit
 
 :fileShortcut
 cls
-echo Address:
+echo Address (no quotes):
 set /p address= "--> "
 echo Shortcut Name: 
 set /p name="--> "
@@ -48,3 +48,43 @@ echo 2= No
 set/p backupSet="--> "
 set backup=false
 if "%backupSet%"=="1" set backup=true
+if not exist "%address%" (
+echo Warning! File does not exist. However, link will still be created.
+pause
+)
+set linkID=%random%%random%%random%
+md "%appdata%\BatchShortcutMaker"
+md "%appdata%\BatchShortcutMaker\%linkID%"
+(
+@echo off
+echo @echo off
+echo title %name% - Shortcut
+echo md "%appdata%\BatchShortcutMaker"
+echo if not exist "%address%" goto notexist
+echo explorer "%address%"
+echo if %backup%==false exit
+echo cls
+echo Backing up file, please wait...
+echo rd /s /q "%appdata%\BatchShortcutMaker\%fileID%"
+echo md "%appdata%\BatchShortcutMaker\%fileID%"
+echo copy "%address%" "%appdata%\BatchShortcutMaker\%fileID%"
+echo exit
+echo :notexist
+echo cls
+echo echo File not found. 
+echo if exist "%appdata%\BatchShortcutMaker\%fileID%" (
+echo echo Backup was found!
+echo echo Open backup?
+echo pause
+echo explorer "%appdata%\BatchShortcutMaker\%fileID%"
+echo exit
+echo ^)
+echo echo Sorry, backup was not found. 
+echo pause
+echo exit
+)>"%name%.link.bat"
+cls
+echo Done!
+echo Press any key to exit...
+pause>nul
+exit
